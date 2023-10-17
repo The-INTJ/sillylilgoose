e0c879d73e9b8035e342af99c23099c5ebdb7f6c
Drew Taylor <drew.taylor@perficient.com>
2023-10-16 21:32:26 -0400

    Add wittle ghostie

--- src/app/Spoopy/page.jsx
+++ src/app/Spoopy/page.jsx
@@ -1,11 +1,14 @@
 'use client'
 import styles from "@/styles/pages/spoopy/Spoopy.module.scss";
+import Image from "next/image";
+import img from "/public/ghostie.png";
 
 
 const SpoopyPage = () => {
   return (
     <div className={styles.spoopy}>
-      <h1>Coming soOoOoOoOn</h1>
+      <Image className={styles.ghostie} src={img} />
+      <h1>and more is Coming soOoOoOoOn</h1>
       <h2>(very spoopy)</h2>
     </div>
   )
